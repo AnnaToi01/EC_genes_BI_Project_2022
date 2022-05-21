67842382b74916d6d201bc2cbb57778110a769c4
Anna Toidze <toidzeanna@gmail.com>
2022-05-21 17:29:53 +0200

    Added remainin tree files, final version of IQTree results

--- Phylogenetic_analysis/tree_drawing.R
+++ Phylogenetic_analysis/tree_drawing.R
@@ -44,8 +44,8 @@ tree_pic <- ggtree(tree_alrt_abayes_ufb, color="black", size=0.5, linetype=1,  r
   scale_color_manual(values=c("black", "orange", "orange"), guide="none") + # Visuals of tip labels
   xlim(0, 15) + # Limit to the length of the branch
   # geom_hilight(node=202, fill="blue", alpha=0.3, extendto=5.63) +
-  geom_hilight(node=206, fill="blue", alpha=0.3, extendto=6.15) + # Highlight EC1.1 clade
-  geom_hilight(node=264, fill="#B589D6", alpha=0.3, extendto=6.15) + # Highlight EC1.2 clade
+  geom_hilight(node=206, fill="blue", alpha=0.3, extendto=6.82) + # Highlight EC1.1 clade
+  geom_hilight(node=262, fill="#B589D6", alpha=0.3, extendto=6.82) + # Highlight EC1.2 clade
   geom_point2(aes(subset=!isTip & node != root, 
                   fill=cut(ufb, c(0, 70, 90, 100))), 
                   shape=21, size=2) + # Create points for bootstrap values
@@ -61,13 +61,13 @@ ggsave("tree_rectangular.png",tree_pic, height = 12, width= 5)
 
 # Circular tree with branch length
 
-ggtree(tree_alrt_abayes_ufb, color="black", size=0.5, linetype=1,  right=TRUE) + 
+tree_branch_length <- ggtree(tree_alrt_abayes_ufb, color="black", size=0.5, linetype=1,  right=TRUE) + 
   layout_circular() + 
   geom_tiplab(aes(color=group), size=1.5, hjust = -0.060, align=T, linesize=0.1) +  xlim(0, 15) +
-  geom_hilight(node=206, fill="blue", alpha=0.3,  extendto=6.15) +
-  geom_cladelabel(node=206, label="EC 1.1", color="blue", offset.text = 1, fontsize = 5, barsize = 1)+
-  geom_hilight(node=264, fill="#B589D6", alpha=0.3,  extendto=6.15) +
-  geom_cladelabel(node=264, label="EC 1.2", color="#8D45A1", offset = -2, offset.text = 0.5, fontsize = 5, barsize = 1)+
+  geom_hilight(node=206, fill="blue", alpha=0.3,  extendto=6.82) +
+  geom_cladelabel(node=206, label="EC1.1", color="blue", offset = 1.55, offset.text = 1, fontsize = 5, barsize = 1)+
+  geom_hilight(node=262, fill="#B589D6", alpha=0.3,  extendto=6.82) +
+  geom_cladelabel(node=262, label="EC1.2", color="#8D45A1", offset = -2, offset.text = 0.5, fontsize = 5, barsize = 1)+
   
   geom_point2(aes(subset=!isTip & node != root, 
                   fill=cut(ufb, c(0, 70, 90, 100))), 
@@ -79,26 +79,8 @@ ggtree(tree_alrt_abayes_ufb, color="black", size=0.5, linetype=1,  right=TRUE) +
                     labels=expression(BP>=90,70 <= BP * " < 90", BP < 70)) +
   scale_color_manual(values=c("black", "red", "red"), guide="none") 
 
+ggsave("tree_circ_branch_length.png",tree_pic, height = 12, width= 5)
 
-# Put back into function
-ggtree(tree_alrt_abayes_ufb, color="black", size=0.5, linetype=1,  right=TRUE, branch.length='none') + 
-  layout_circular() + 
-  geom_tiplab(aes(color=group), size=5, hjust = -0.060, align=T, linesize=0.1) +  
-  geom_hilight(node=node_1, fill="blue", alpha=0.3,  extendto=6.15) +
-  geom_cladelabel(node=node_1, label="EC1.1", color="#0000FF", offset=offset_1, offset.text = -1.5, fontsize = 15, barsize = 1, geom='label', fill='#a3a3f7')+
-  geom_hilight(node=node_2, fill="#B589D6", alpha=0.3,  extendto=6.15) +
-  geom_cladelabel(node=node_2, label="EC1.2", color="#8D45A1", offset = offset_2, offset.text = -0.5, fontsize = 15, barsize = 1, geom='label',  fill='#dba2eb')+
-  geom_point2(aes(subset=!isTip & node != root, 
-                  fill=cut(ufb, c(0, 70, 90, 100))), 
-              shape=21, size=10) + 
-  theme_tree(legend.position=c(0.9, 0.9)) +
-  theme(legend.title=element_text(size=30, face='bold'), 
-        legend.text=element_text(size=25)) +
-  scale_fill_manual(values=c("white", "grey", "black"), guide='legend', 
-                    name='Bootstrap Percentage', 
-                    breaks=c('(90,100]', '(70,90]', '(0,70]'), 
-                    labels=expression(BP>=90,70 <= BP * " < 90", BP < 70)) +
-  scale_color_manual(values=c("black", "red", "red"), guide="none") 
 
 
 # Define function to draw tree picture without branch length and save it
@@ -134,7 +116,7 @@ save_tree_without_branchlength <- function(input_file, output_file, node_1, node
     geom_point2(aes(subset=!isTip & node != root, 
                     fill=cut(ufb, c(0, 70, 90, 100))), 
                 shape=21, size=10) + 
-    theme_tree(legend.position=c(0.9, 0.9)) +
+    theme_tree(legend.position=c(0.95, 0.95)) +
     theme(legend.title=element_text(size=30, face='bold'), 
           legend.text=element_text(size=25)) +
     scale_fill_manual(values=c("white", "grey", "black"), guide='legend', 
@@ -149,11 +131,11 @@ save_tree_without_branchlength <- function(input_file, output_file, node_1, node
 }
 
 # Circular tree picture without branch length for GitHub
-save_tree_without_branchlength("clustalo_not_trimmed_iqtree_bootstrap.treefile", "tree_without_branch_length.png", 206, 264, -16.5, -17.5)
+save_tree_without_branchlength("clustalo_not_trimmed_iqtree_bootstrap.treefile", "tree_circ_without_branch_length.png", 206, 262, -15.55, -19.6)
 
 # Save the gappyout trimmed trees the same way
-save_tree_without_branchlength("clustalo_gappyout_trimmed_iqtree_bootstrap.treefile", "gappyout_trimmed_tree_without_branch_length.png", 366, 256, -10.5, -17.5)
+save_tree_without_branchlength("clustalo_gappyout_trimmed_iqtree_bootstrap.treefile", "gappyout_trimmed_tree_circ_without_branch_length.png", 366, 256, -10.5, -17.5)
 
 # Save the gappyout trimmed trees the same way
-save_tree_without_branchlength("clustalo_strict_trimmed_iqtree_bootstrap.treefile", "strict_trimmed_tree_without_branch_length.png", 355, 245, -11.5, -19.5)
+save_tree_without_branchlength("clustalo_strict_trimmed_iqtree_bootstrap.treefile", "strict_trimmed_tree_circ_without_branch_length.png", 355, 245, -11.5, -19.5)
 
